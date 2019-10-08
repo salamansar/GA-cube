3039f265abae2bee5b09bf43cb014cc882dc0c82
Salamansar <salamansar@gmail.com>
2019-10-08 07:29:52 +0300

    Extract reading/writing graph into separate module

---
+++ src/GraphIO.hs
@@ -0,0 +1,52 @@
+module GraphIO where
+
+import Data.Graph
+import Data.GraphViz
+import Data.GraphViz.Commands.IO
+import Data.List
+import GaGraph
+
+
+-- Reading Graph from .dot file
+readGraph :: String -> IO (Graph, Int)
+readGraph path = do
+   g <- (readDotFile path)::IO (DotGraph Int)
+   putStrLn "Read graph is:"
+   putDot g
+   let (mn, mx) = graphBounds g
+   let edges = dotGraphEdges mn g
+   let inGraph = buildG (0, mx - mn) edges
+   return (inGraph, mn)
+   
+-- Write vertexes into PNG image file with adjust rate
+writeVertexes :: String -> Int -> [Vertex] -> IO ()
+writeVertexes path adjustRate vertexes = do
+   let dg = graphFromNodes adjustRate vertexes
+   putStrLn "Written graph is:\n"
+   putDot dg
+   runGraphvizCommand Dot dg Png path
+   return ()
+
+
+--------- helper functions------------
+   
+graphBounds :: DotGraph Int -> Bounds
+graphBounds g = let nodes = map nodeID (graphNodes g)
+                in (minimum nodes, maximum nodes)
+                
+dotGraphEdges :: Int -> DotGraph Int -> [Edge]
+dotGraphEdges adjRate = (adjustNodes adjRate)
+                        .withBackwards
+                        .(map toEdge)
+                        .graphEdges
+   where toEdge de = (fromNode de, toNode de)
+         withBackwards srcEdges = srcEdges ++ (map (\(a,b) -> (b,a)) srcEdges)
+         adjustNodes adjRate = map (\(a,b) -> (a - adjRate, b - adjRate))
+
+graphFromNodes :: Int -> [Vertex] -> DotGraph Int
+graphFromNodes adjRate v = 
+   let adjV = map (+adjRate) v 
+   in graphElemsToDot params (createNodes adjV) (createEdges adjV)
+      where params = quickParams::GraphvizParams Int Int Int () Int
+            createNodes = (map (\x -> (x,x))).nub 
+            createEdges vx = zipWith (\(a,b) c -> (a,b,c)) (formEdges vx) [1..]  
--- src/Main.hs
+++ src/Main.hs
@@ -2,29 +2,19 @@ module Main where
 
 import GaGraph
 import Data.Graph
-import Data.GraphViz
-import Data.GraphViz.Commands.IO
-import Data.GraphViz.Types
 import Data.List
 import qualified EilerGenAlg as Ega
 import EilerGraph
 import qualified GenAlg2 as Ga2
 import GenAlgEngine
 import GraphSamples
+import GraphIO
 
 main::IO ()
 main = do
-   g <- (readDotFile "srcGraph.dot")::IO (DotGraph Int)
-   putStrLn "Source graph is:"
-   putDot g
-   let (mn, mx) = graphBounds g
-   let edges = dotGraphEdges mn g
-   let inGraph = buildG (0, mx - mn) edges
-   let eilerResult = getEilerResult inGraph
-   let rg = graphFromNodes mn eilerResult
-   putStrLn "Result graph is:\n"
-   putDot rg
-   runGraphvizCommand Dot rg Png "resultGraph.png"
+   (g, adj) <- readGraph "srcGraph.dot"
+   let eilerResult = getEilerResult g
+   writeVertexes "resultGraph.png" adj eilerResult
    return ()
 
 getOnesResult :: String
@@ -34,25 +24,4 @@ getEilerResult :: Graph -> [Int]
 getEilerResult g = let (count, result) = runGA Ega.defaultGenAlgContext (Ega.eilerFirstGen g 128) (Ega.eilerPathFound g)
    in case find Ega.isEilerPath result of
       Nothing -> []
-      Just eg -> eilerPhenotype eg
-
-graphBounds :: DotGraph Int -> Bounds
-graphBounds g = let nodes = map nodeID (graphNodes g)
-                in (minimum nodes, maximum nodes)
-                
-dotGraphEdges :: Int -> DotGraph Int -> [Edge]
-dotGraphEdges adjRate = (adjustNodes adjRate)
-                        .withBackwards
-                        .(map toEdge)
-                        .graphEdges
-   where toEdge de = (fromNode de, toNode de)
-         withBackwards srcEdges = srcEdges ++ (map (\(a,b) -> (b,a)) srcEdges)
-         adjustNodes adjRate = map (\(a,b) -> (a - adjRate, b - adjRate))
-
-graphFromNodes :: Int -> [Vertex] -> DotGraph Int
-graphFromNodes adjRate v = 
-   let adjV = map (+adjRate) v 
-   in graphElemsToDot params (createNodes adjV) (createEdges adjV)
-      where params = quickParams::GraphvizParams Int Int Int () Int
-            createNodes = (map (\x -> (x,x))).nub 
-            createEdges vx = zipWith (\(a,b) c -> (a,b,c)) (formEdges vx) [1..]  
+      Just eg -> eilerPhenotype eg 
