4014e860290e99c29f2381d079d8546140220a3b
Salamansar <salamansar@gmail.com>
2020-06-21 20:07:47 +0300

    Using optparse-applicative for cmd args parsing

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,6 @@
 {-#LANGUAGE FlexibleContexts #-}
 module Main where
 
-import Control.Exception.Base
 import Control.Monad.Except
 import GaGraph
 import Data.Graph
@@ -13,118 +12,92 @@ import EilerGraph
 import qualified GenAlg2 as Ga2
 import GenAlgEngine
 import GraphIO
-import System.Console.GetOpt
-import System.Environment
 import EilerServer
 import qualified Data.Text.Lazy as T
 import Data.GraphViz
+import Options.Applicative
 
 type GraphArgs = (String, String)
 type ServerArgs = (String, Int)
-data CmdArgs = CmdArgs {
-   graphArgs::Maybe GraphArgs,
-   isInteractive::Bool,
-   isHelpRequested::Bool,
-   isServerMode::Bool,
-   isClientMode::Bool,
-   serverArgs::Maybe ServerArgs
-} deriving Show
 
-emptyArgs = CmdArgs { 
-   graphArgs = Nothing,
-   isInteractive = False,
-   isHelpRequested = False,
-   isServerMode = False,
-   isClientMode = False,
-   serverArgs = Nothing
-   }
-
+data CmdArgs = Interactive
+              | LocalArgs GraphArgs
+              | ServerMode Int
+              | ClientMode ServerArgs GraphArgs 
+              deriving Show               
+             
 main::IO ()
 main = do 
-   args <- getArgs
-   case readCmdArgs args of
-      Right parsedArgs -> handleCmdArgs parsedArgs
-      Left e -> putStrLn e
+   args <- execParser cmdArgsInfo
+   handleCmdArgs args
 
-readCmdArgs :: [String] -> Either String CmdArgs
-readCmdArgs args = do
-   cmdArgs <- parseCmdArgs args
-   case cmdArgs of
-      CmdArgs { isHelpRequested = True, 
-                graphArgs = Nothing, 
-                isInteractive = False, 
-                isClientMode = False, 
-                isServerMode = False, 
-                serverArgs = Nothing 
-              } -> return cmdArgs
-      CmdArgs { isHelpRequested = False, 
-                graphArgs = Just (i,o), 
-                isInteractive = False,
-                isClientMode = False, 
-                isServerMode = False, 
-                serverArgs = Nothing 
-              } | (i /= "") && (o /= "") -> return cmdArgs
-      CmdArgs { isHelpRequested = False, 
-                graphArgs = Just (i,o), 
-                isInteractive = False,
-                isClientMode = True, 
-                isServerMode = False, 
-                serverArgs = Just (h,p) 
-              } | (h /= "") && (p /= 0) && (i /= "") && (o /= "") -> return cmdArgs
-      CmdArgs { isHelpRequested = False, 
-                graphArgs = Nothing, 
-                isInteractive = False,
-                isClientMode = False, 
-                isServerMode = True, 
-                serverArgs = Just (h,p) 
-              } | (h == "") && (p /= 0) -> return cmdArgs
-      CmdArgs { isHelpRequested = False, 
-                graphArgs = Nothing, 
-                isInteractive = True,
-                isClientMode = False, 
-                isServerMode = False, 
-                serverArgs = Nothing 
-              } -> return cmdArgs
-      otherwise -> throwError "Wrong combination of input arguments"
+------ cmd args parsing -------------
+cmdArgsInfo :: ParserInfo CmdArgs
+cmdArgsInfo = info (cmdArgsParser <**> helper) (fullDesc <> header "Eiler path evaluator")
 
-parseCmdArgs :: MonadError String m => [String] -> m CmdArgs
-parseCmdArgs [] = return emptyArgs { isInteractive = True }
-parseCmdArgs args = case getOpt Permute cmdOptions args of
-      (optsF, [], []) -> return $ foldr id emptyArgs optsF
-      (_, _, e) -> throwError $ "Error in arguments parsing. " ++ (concat e)
-      
-cmdOptions :: [OptDescr (CmdArgs -> CmdArgs)]
-cmdOptions = [
-                Option ['i'] ["input"] (OptArg (parseIO fillInput) "INPUT") "Input graph .dot filename",
-                Option ['o'] ["output"] (OptArg (parseIO fillOutput) "OUTPUT") "Output PNG filename",                
-                Option ['h'] ["help"] (NoArg (\opts -> opts { isHelpRequested = True })) "Print help",
-                Option ['c'] ["client"] (NoArg (\opts -> opts { isClientMode = True })) "Client mode",
-                Option ['s'] ["server"] (NoArg (\opts -> opts { isServerMode = True })) "Server mode",
-                Option ['l'] ["location"] (OptArg (parseIO fillHost) "HOST") "Location/host of server",
-                Option ['p'] ["port"] (OptArg (parseIO fillPort) "PORT") "Port of server"
-             ]
-   where parseIO f p opts = maybe opts (f opts) p
-         fillInput opts@CmdArgs{graphArgs=ga} p = case ga of 
-                                                     Just (i,o) -> opts { graphArgs = Just (p,o) }
-                                                     Nothing -> opts { graphArgs = Just (p, "") }
-         fillOutput opts@CmdArgs{graphArgs=ga} p = case ga of 
-                                                      Just (i,o) -> opts { graphArgs = Just (i,p) }
-                                                      Nothing -> opts { graphArgs = Just ("", p) }
-         fillHost opts@CmdArgs{serverArgs=sa} pr = case sa of 
-                                                      Just (h,p) -> opts { serverArgs = Just (pr,p) }
-                                                      Nothing -> opts { serverArgs = Just (pr, 0) }
-         fillPort opts@CmdArgs{serverArgs=sa} pr = let port = read pr in 
-                                                      case sa of 
-                                                         Just (h,p) -> opts { serverArgs = Just (h,port) }
-                                                         Nothing -> opts { serverArgs = Just ("", port) }
-      
+cmdArgsParser :: Parser CmdArgs
+cmdArgsParser = localArgsParser <|> serverModeParser <|> clientModeParser <|> interactiveArgsParser
+
+interactiveArgsParser :: Parser CmdArgs
+interactiveArgsParser = pure Interactive
+
+localArgsParser :: Parser CmdArgs
+localArgsParser = LocalArgs <$> graphArgsParser
+
+graphArgsParser :: Parser GraphArgs
+graphArgsParser = (,) 
+               <$> strOption (
+                      long "input"
+                      <> short 'i'
+                      <> metavar "INPUT"
+                      <> help "Input graph .dot filename"    
+                   )
+              <*> strOption (
+                      long "output"
+                      <> short 'o'
+                      <> metavar "OUTPUT"
+                      <> help "Output PNG filename"    
+                  )
+
+serverModeParser :: Parser CmdArgs
+serverModeParser = flag' () (
+                      long "server"
+                      <> short 's'
+                      <> help "Server mode"    
+                   ) *> (ServerMode <$> portOption)
+
+clientModeParser :: Parser CmdArgs
+clientModeParser = flag' () (
+                      long "client"
+                      <> short 'c'
+                      <> help "Client mode"    
+                   ) *> (ClientMode <$> serverArgsParser <*> graphArgsParser)
+
+serverArgsParser :: Parser ServerArgs
+serverArgsParser = (,) 
+               <$> strOption (
+                      long "location"
+                      <> short 'l'
+                      <> metavar "LOCATION"
+                      <> help "Location/host of server"    
+                   )
+              <*> portOption
+ 
+portOption :: Parser Int 
+portOption = option auto (
+                      long "port"
+                      <> short 'p'
+                      <> metavar "PORT"
+                      <> help "Port of server"    
+                  )      
+
+----------- handling -------------      
 handleCmdArgs :: CmdArgs -> IO()
-handleCmdArgs args = do 
-   case args of
-      CmdArgs { isHelpRequested = True } -> putStrLn getUsageInfo
-      CmdArgs { isServerMode = True, serverArgs = Just (_,p) } -> startEilerGaServer p
-      CmdArgs { isClientMode = True, graphArgs = Just ga, serverArgs = Just sa } -> startEilerGaClient ga sa
-      otherwise -> handleGaArgs args
+handleCmdArgs Interactive = readGraphParamsInteractive >>= handleLocalGraphArgs
+handleCmdArgs (LocalArgs grArgs) = handleLocalGraphArgs grArgs
+handleCmdArgs (ServerMode port) = startEilerGaServer port
+handleCmdArgs (ClientMode srvArgs grArgs) = startEilerGaClient grArgs srvArgs
+
    
 ----- server mode --------   
 startEilerGaServer :: Int -> IO ()
@@ -139,21 +112,14 @@ startEilerGaClient (input, output) serverArgs = do putStrLn $ "Reading grpah fro
                                                    result <- receiveEilerResult (T.pack inputGraph) serverArgs
                                                    putStrLn $ "Writing result to PNG image " ++ output
                                                    exportToPng (parseDotGraph result) output   
-----------------------------------
-                                            
-handleGaArgs :: CmdArgs -> IO()
-handleGaArgs args = do 
-   (srcPath, destPath) <- case args of
-      CmdArgs { isInteractive = True } -> readGraphParamsInteractive
-      CmdArgs { graphArgs = Just (i,o) } -> return (i, o)
+----------------------------------                                            
+handleLocalGraphArgs :: GraphArgs -> IO ()
+handleLocalGraphArgs (srcPath, destPath) = do
    (g, adj) <- readGraph srcPath
    case getEilerResult g of
       Left e -> putStrLn e
       Right result -> do writeVertexes destPath adj result
-                         putStrLn $ "Result was written into " ++ destPath
- 
-getUsageInfo :: String
-getUsageInfo = usageInfo "Usage: \n\tEmpty args - interactive mode\n\tParams from cmd: -i[INPUT] -o[OUTPUT]\n\tHelp: -h" cmdOptions                                           
+                         putStrLn $ "Result was written into " ++ destPath                                     
 
 readGraphParamsInteractive :: IO GraphArgs
 readGraphParamsInteractive = do 
